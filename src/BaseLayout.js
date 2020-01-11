a06b8e6915ce3f6a774994f96a035e494df60968
Konstantin6487 <Konstantin6487@rambler.ru>
2020-01-11 23:10:30 +0300

    base layout

---
+++ src/BaseLayout.js
@@ -0,0 +1,53 @@
+export default class BaseLayout {
+  constructor(element) {
+    this.element = element;
+  }
+
+  template = `
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary w-100">
+          <span class="navbar-brand">RSS reader</span>
+        </nav>
+      </div>
+      <div class="row">
+        <div class="col-sm-2 px-0 bg-dark min-vh-100">
+          <div class="sticky-top pt-1">
+            <span class="pl-3 font-weight-bold text-muted font-italic border-0">Channels</span>
+            <ul class="nav nav-tabs flex-sm-column border-0"></ul>
+          </div>
+        </div>
+        <main class="col p-0" role="main">
+          <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+              <h1>RSS Preview</h1>
+              <p class="lead">A simple online RSS reader.<br />Enter the URL of the RSS feed, click sync, and preview the results.</p>
+              <form>
+                <label for="feedUrl"><b>Link</b></label>
+                <div class="input-group">
+                  <input type="text" name="feed-url" class="form-control form-control-lg" id="feedUrl" placeholder="RSS Feed URL" aria-describedby="emailHelp">
+                  <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit" id="button-submit">Sync</button>
+                  </div>
+                  <div class="feedback"></div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <section class="container">
+            <ul class="list-group">
+              <li class="list-group-item mb-2">This is a danger list group item</li>
+              <li class="list-group-item mb-2">This is a warning list group item</li>
+              <li class="list-group-item mb-2">This is a info list group item</li>
+              <li class="list-group-item mb-2">This is a light list group item</li>
+              <li class="list-group-item mb-2">This is a dark list group item</li>
+            </ul>
+          </section>
+        </main>
+      </div>
+    </div>`;
+
+  init() {
+    this.element.innerHTML = this.template;
+  }
+}
