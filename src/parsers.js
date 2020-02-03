9c29d314fe64244e2e141fb7b6c4328cb8a78b6f
Konstantin6487 <Konstantin6487@rambler.ru>
2020-02-04 01:43:35 +0300

    add parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,3 +1,5 @@
+import { uniqueId } from 'lodash-es';
+
 const getDOMParser = () => {
   const parser = new DOMParser();
   const { parseFromString } = parser;
@@ -20,7 +22,7 @@ const parsers = {
   'image/svg+xml': getDOMParser,
 };
 
-const getParser = (mimeType) => {
+const getDOMStrParser = (mimeType) => {
   const createParseFn = parsers[mimeType];
   if (!createParseFn) {
     throw new Error(`unknown mime type: ${mimeType}`);
@@ -28,8 +30,42 @@ const getParser = (mimeType) => {
   return createParseFn();
 };
 
-export default (data, mimeType) => {
-  const parse = getParser(mimeType);
+export const parseDOMStr = (data, mimeType) => {
+  const parse = getDOMStrParser(mimeType);
   const parsed = parse(data, mimeType);
   return parsed;
 };
+
+export const parseToChannelData = (model, { maxId, feedURL }) => {
+  const channel = model.querySelector('channel');
+  if (!channel) {
+    throw new Error('Parsing error');
+  }
+  const title = model.querySelector('channel > title').textContent;
+  const description = model.querySelector('channel > description').textContent;
+  const channelId = maxId + +uniqueId();
+  const channelData = {
+    link: feedURL,
+    title,
+    description,
+    id: channelId,
+  };
+  return channelData;
+};
+
+export const parseToChannelItems = (model, { maxId, channelId }) => Array
+  .from(model.querySelectorAll('channel > item'))
+  .map((item) => {
+    const link = item.querySelector('link').textContent;
+    const title = item.querySelector('title').textContent;
+    const description = item.querySelector('description').textContent;
+    const itemId = maxId + uniqueId();
+    const itemData = {
+      link,
+      title,
+      description,
+      channelId,
+      id: itemId,
+    };
+    return itemData;
+  });
