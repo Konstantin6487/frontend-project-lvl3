302daa34fcd1eba22591e981ceeac1bdcc839bb3
Konstantin6487 <Konstantin6487@rambler.ru>
2020-02-16 12:35:33 +0300

    mv selectors

--- src/application.js
+++ src/application.js
@@ -7,6 +7,7 @@ import {
 } from 'lodash-es';
 import { watch } from 'melanke-watchjs';
 import { isURL } from 'validator';
+import getSelectors from './selectors';
 import {
   parse,
   selectChannelContent,
@@ -32,16 +33,7 @@ export default () => {
     maxId: 0,
   };
 
-  const selectors = {
-    sidebar: document.querySelector('#sidebar'),
-    navTabs: document.querySelector('.nav-tabs'),
-    itemsContainer: document.querySelector('.items-container > ul'),
-    form: document.querySelector('form'),
-    input: document.querySelector('#feedUrl'),
-    submitBtn: document.querySelector('#button-submit'),
-    modal: document.querySelector('#channelItemModal'),
-    feedback: document.querySelector('.feedback'),
-  };
+  const selectors = getSelectors();
 
   const { form, modal, input } = selectors;
 
---
+++ src/selectors.js
@@ -0,0 +1,10 @@
+export default () => ({
+  sidebar: document.querySelector('#sidebar'),
+  navTabs: document.querySelector('.nav-tabs'),
+  itemsContainer: document.querySelector('.items-container > ul'),
+  form: document.querySelector('form'),
+  input: document.querySelector('#feedUrl'),
+  submitBtn: document.querySelector('#button-submit'),
+  modal: document.querySelector('#channelItemModal'),
+  feedback: document.querySelector('.feedback'),
+});
