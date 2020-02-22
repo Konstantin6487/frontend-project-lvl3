f79aa6ab92a8b6aa18995902390e755529fb4cdb
Bochinin Konstantin <k.bochinin@rambler-co.ru>
2020-02-22 20:11:37 +0300

    add i18n preload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import i18n from 'i18next';
+
 import 'bootstrap';
 import '../scss/app.scss';
-import './lib/i18n';
 
 import run from './application';
 
-run();
+import enTranslation from '../locales/en/translation.json';
+
+i18n.init({
+  lng: 'en',
+  debug: true,
+  resources: {
+    en: {
+      translation: enTranslation,
+    },
+  },
+}).then(run);
