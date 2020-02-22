74fe6a3f76c330d0ee38f433f9f271cbd75df333
Bochinin Konstantin <k.bochinin@rambler-co.ru>
2020-02-22 17:57:10 +0300

    add validation fn

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
 const corsProxyUrl = 'https://cors-anywhere.herokuapp.com';
 
-export default (url) => `${corsProxyUrl}/${url}`;
+export const buildUrl = (url) => `${corsProxyUrl}/${url}`;
+
+export const validate = (value, constraints) => {
+  const checkedForInvalid = constraints.find(({ check }) => check(value));
+  if (checkedForInvalid) {
+    return ({ isValid: false, message: checkedForInvalid.message });
+  }
+  return ({ isValid: true });
+};
